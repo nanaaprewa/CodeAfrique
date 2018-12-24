ce77b83d87325b0fda05d6a23923f9a7a9b29279
Cynoc <boahenecynoc@gmail.com>
2018-12-24 09:40:08 +0000

    Added score and high score computation.
    Fixed some bugs,  added new features, and played around with some colors. Also added score and high score computation

--- Snake.py
+++ Snake.py
@@ -16,62 +16,101 @@ want to go left.
 '''
 import random
 import curses
+import time
 
 s = curses.initscr()
+curses.start_color()
 curses.curs_set(0)
 sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
+w = curses.newwin(sh+1, sw+1, 0, 0)
 w.keypad(1)
 w.timeout(100)
-DOWN_ARROW = curses.KEY_DOWN
-UP_ARROW = curses.KEY_UP
-RIGHT_ARROW = curses.KEY_RIGHT
-LEFT_ARROW = curses.KEY_LEFT
+maxScore = 0
+score = 0
 snk_x = sw/4
 snk_y = sh/2
-
+f = open("high score","a+")
+f.close()
 snake = [
     [snk_y, snk_x],
     [snk_y, snk_x-1],
     [snk_y, snk_x-2],
     [snk_y, snk_x-3]
 ]
+
 food = [sh/2, sw/2]
 w.addch(food[0], food[1], curses.ACS_PI)
 
 key = curses.KEY_RIGHT
+color = random.randint(1,6)
+curses.init_pair(1, color, curses.COLOR_WHITE)
+curses.init_pair(2, 2, curses.COLOR_WHITE)
+
+with open("high score","r") as f:
+    scores = [int(i)  for j in f for i in j.strip().split(" ") if i.isdigit()]
+f.close()
+if len(scores) > 0:
+    maxScore = max(scores)
 
 while True:
-    ## get key pressed
+    w.addstr(0, 2, 'Score : ' + str(score) + ' ',curses.color_pair(1))
+    w.addstr(0, 20, 'High Score : ' + str(maxScore) + ' ',curses.color_pair(2))
+
+    prevKey = key
+    #Increases the speed of Snake as its length increases
+    w.timeout(100 - (len(snake)/5 + len(snake)/10)%120)
+
     next_key = w.getch()
     key = key if next_key == -1 else next_key
-    ## Quit game if the snake hits any of the walls
-    if snake[0][0] in [0, sh] or snake[0][1] in [0, sw] or snake[0] in snake[1:]:
+
+    if key == ord(' '):          # If SPACE BAR is pressed, wait for another
+        key = -1                 # one (Pause/Resume)
+        while key != ord(' '):
+            key = w.getch()
+        key = prevKey
+        continue
+         # If an invalid key is pressed
+    if key not in [ curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, 27]:
+        key = prevKey
+
+### DO NOT CHANGE CODE ABOVE
+
+### Students are responsible for implementing the commented out  code
+    new_head = [snake[0][0], snake[0][1]]
+    if key == curses.KEY_DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY_LEFT:
+        new_head[1] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
+
+    ### DO NOT CHANGE CODE BELOW
+    snake.insert(0, new_head)
+
+     # If snake crosses the boundaries, make it enter from the other side
+    if snake[0][0] == 0: snake[0][0] = sh-1
+    if snake[0][1] == 0: snake[0][1] = sw -1
+    if snake[0][0] == sh: snake[0][0] = 1
+    if snake[0][1] == sw: snake[0][1] = 1
+
+
+    if snake[0] in snake[1:]:
+        file = open('high score', 'a+')
+        file.write(str(score) + '\n')
+        file.close()
+        time.sleep(1)
         curses.endwin()
         quit()
-    new_head = [snake[0][0], snake[0][1]]
-    ###DONOT CHANGE CODE ABOVE
-    ##Implement me (Students will be responsible for implementing the commented
-    ##out section)
-
-    # if key == DOWN_ARROW:
-    #     new_head[0] += 1
-    # if key == UP_ARROW:
-    #     new_head[0] -= 1
-    # if key == LEFT_ARROW:
-    #     new_head[1] -= 1
-    # if key == RIGHT_ARROW:
-    #     new_head[1] += 1
-
-    ##DONOT CHANGE CODE BELOW
-    snake.insert(0, new_head)
 
     if snake[0] == food:
         food = None
+        score += 1
         while food is None:
             nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sh-1)
+                random.randint(1, sh-2),
+                random.randint(1, sh-2)
             ]
             food = nf if nf not in snake else None
         w.addch(food[0], food[1], curses.ACS_PI)
